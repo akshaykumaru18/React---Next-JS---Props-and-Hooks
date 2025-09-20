dc42f96c6a15d41751afa47a42661fb21ade4163
Akshay Kumar U <akshayapsangi123@gmail.com>
2025-09-20 19:47:48 +0530

    Prop Demo

--- app/page.js
+++ app/page.js
@@ -1,103 +1,125 @@
-import Image from "next/image";
+"use client";
+import BookCard from "./components/BookCard"
+import {useState} from 'react'
+export default function Home(){
+  //list = [1,2,3,4,5,6,7]
+  //JSON (Javascript object notation)
+ 
+   const [cart, setCart]= useState([]);
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const books = [
+    {
+      title: "Programming with Java",
+      desc : "We have 200+ books related to Programming",
+      img: "https://m.media-amazon.com/images/I/61FmuzUH8AL._UF1000,1000_QL80_.jpg"
+    },
+    {
+      title: "Design and Analysis of Algorithms",
+      desc : "How to work with c and DSA",
+      img: "https://m.media-amazon.com/images/I/714+tgyHDRL._UF1000,1000_QL80_.jpg"
+    },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  
+  return <div>
+    <center style={{marginTop: "40px", fontWeight: "bold", fontSize: "40px"}}>
+      Sahyadri Digital Library
+    </center>
+
+    <div>Cart items</div>
+    {
+      cart.map((element)=> {
+         return <BookCard 
+         key={element.desc}
+        title={element.title}
+        desc={element.desc}
+        bookImg={element.img}
+          onBorrowClick={(e)=>{
+            cart.push(element);
+        }}
+     />
+      })
+    }
+     <div>Store items</div>
+    {
+      books.map((element)=> {
+         return <BookCard 
+         key={element.desc}
+        title={element.title}
+        desc={element.desc}
+        bookImg={element.img}
+          onBorrowClick={(e)=>{
+            //JAVASCRIPT SPREAD OPERATER
+            setCart([...cart,element])
+            console.log(`Cart Items : ${cart}`)
+        }}
+     />
+      })
+    }
+
+    <CategoryTitle title= {"Programming Books"} 
+    desc = {"We have 200+ books related to Programming"}/>
+    <div style={{
+      display:"flex",
+      flexDirection:"row"
+    }}>
+    <BookCard 
+    title={"Programming with Java"}
+    desc={"How to work with java as beginner"
+    }
+    bookImg={"https://m.media-amazon.com/images/I/61FmuzUH8AL._UF1000,1000_QL80_.jpg"}
+      onBorrowClick={(e)=>{
+        alert('How are you')
+     }}
+    />
+
+
+     <BookCard 
+    title={"Design and Analysis of Algorithms"}
+    desc={"How to work with c and DSA"
+    }
+    bookImg={"https://m.media-amazon.com/images/I/714+tgyHDRL._UF1000,1000_QL80_.jpg"}/>
     </div>
-  );
+   
+    <CategoryTitle title = {"Business Books"}/>
+      <BookCard 
+    title={"Design and Analysis of Algorithms"}
+    desc={"How to work with c and DSA"
+    }
+    bookImg={"https://m.media-amazon.com/images/I/714+tgyHDRL._UF1000,1000_QL80_.jpg"}/>
+      <BookCard 
+    title={"Design and Analysis of Algorithms"}
+    desc={"How to work with c and DSA"
+    }
+    bookImg={"https://m.media-amazon.com/images/I/714+tgyHDRL._UF1000,1000_QL80_.jpg"}/>
+    <CategoryTitle title = {"Self Help Books"}/>
+      <BookCard 
+    title={"Design and Analysis of Algorithms"}
+    desc={"How to work with c and DSA"
+    }
+    bookImg={"https://m.media-amazon.com/images/I/714+tgyHDRL._UF1000,1000_QL80_.jpg"}/>
+      <BookCard 
+    title={"Design and Analysis of Algorithms"}
+    desc={"How to work with c and DSA"
+    }
+    bookImg={"https://m.media-amazon.com/images/I/714+tgyHDRL._UF1000,1000_QL80_.jpg"}/>
+  </div>
 }
+
+// {}
+// Properties passing or Prop Passing
+function CategoryTitle({title,desc}){
+  return (
+   <div style={{marginLeft : "20px",}}>
+     <div style={{ fontFamily:"mono-space",fontSize: "20px"}}>
+       {title}
+    </div>
+    <div style={{fontSize: "10px"}}>
+      {desc}
+    </div>
+    <hr />
+   </div>
+  )
+}
+
+
